3ae02f8482e395f0f04d6ddcce4884a320681021
Alexey Papin <zzheads@gmail.com>
2016-09-02 08:52:30 +0300

    Final commit // DONE: Ensure unit tests pass.

--- src/main/java/com/teamtreehouse/techdegree/Main.java
+++ src/main/java/com/teamtreehouse/techdegree/Main.java
@@ -4,7 +4,7 @@ import com.teamtreehouse.techdegree.hardware.FrightMachine;
 
 // DONE: Use the Observer design pattern to allow all accessories to be plugged in.
 // DONE: Use another design pattern to adapt the camera to be an accessory. // Adapter?
-// TODO: Ensure unit tests pass.
+// DONE: Ensure unit tests pass.
 
 public class Main {
     public static void main(String[] args) {
